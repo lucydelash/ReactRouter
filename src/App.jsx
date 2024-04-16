406f01f0b856de16176ecf71b79d7b0c88e10b2c
lucydelash <lucyrudd614@gmail.com>
2024-04-16 14:53:13 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+    <div id="container">
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App;
